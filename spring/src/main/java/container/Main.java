65fadde6cf0c39557eea8bd9589f602f0927cc58
zhuangjinxin <zhuangjinxin@hotmail.com>
2020-05-10 13:59:12 +0800

    add git ignore

---
+++ spring/src/main/java/container/Main.java
@@ -0,0 +1,13 @@
+package container;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Bean bean = context.getBean("bean", Bean.class);
+        System.out.println(bean.getParam());
+    }
+}
